ae2112ba847f747e641ad9a587b1e1f224be659b
timof <timofejstroev@gmail.com>
2026-04-27 18:06:56 +0300

    add: main.py feat: add asynchronous factorial program

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import asyncio
+
+def factorial_sync(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
+
+async def factorial_async(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+        await asyncio.sleep(0)
+    return result
+
 n = int(input())
-factorial = 1
 
-for i in range(1, n + 1):
-    factorial = factorial * i
-
-print(f"{n}! = {factorial}")
+print(factorial_sync(n))
+print(asyncio.run(factorial_async(n)))
