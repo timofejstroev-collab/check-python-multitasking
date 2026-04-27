21aeb2b36714b9d91fe0c0a42eadc67a367963dc
timof <timofejstroev@gmail.com>
2026-04-27 19:11:04 +0300

    feat: adв factorial-without-gil program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 def factorial_sync(n):
     result = 1
@@ -13,7 +14,19 @@ async def factorial_async(n):
         await asyncio.sleep(0)
     return result
 
+def factorial_worker(n, results, index):
+    results[index] = factorial_sync(n)
+
+def factorial_without_gil(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+        time.sleep(0.0001)  # Спим, отдаём GIL другим потокам
+    return result
+
+
 n = int(input())
 
 print(factorial_sync(n))
 print(asyncio.run(factorial_async(n)))
+print(factorial_without_gil(n))
